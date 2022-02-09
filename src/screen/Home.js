ec32f12c7118eb9d728903e98e06384ced5ea16c
sannimdev <inegg.apps@gmail.com>
2022-02-09 21:53:59 +0900

    헬멧

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,9 +1,11 @@
 import Container from '../components/home/header/Container';
 import Header from '../components/home/header/Header';
+import HelmetTitle from '../components/HelmetTitle';
 
 function Home() {
     return (
         <Container>
+            <HelmetTitle title={'카페'} />
             <Header gnbList={['카페']} />
         </Container>
     );

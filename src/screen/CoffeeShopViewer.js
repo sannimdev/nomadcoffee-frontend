cfa605e69cca0fe2c8684c50791217f266999697
Hojoong Choi <inegg.apps@gmail.com>
2022-02-11 13:33:43 +0900

    Deploying for netlify

--- src/screen/CoffeeShopViewer.js
+++ src/screen/CoffeeShopViewer.js
@@ -1,8 +1,8 @@
 import { gql, useMutation, useQuery, useReactiveVar } from '@apollo/client';
-import { faEraser, faPlus, faTrash, faXRay } from '@fortawesome/free-solid-svg-icons';
+import { faEraser, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
 import { useForm } from 'react-hook-form';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled, { ThemeContext } from 'styled-components';
 import { gnbListVar } from '../apollo';
 import HelmetTitle from '../components/HelmetTitle';
